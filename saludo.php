991c8f31ec17c4fa52bd7d4a5a0a949b73eea52d
gustavo1017 <gustavoeqr1017@gmail.com>
2018-07-09 18:15:05 -0500

    Vistas html

---
+++ saludo.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Saludo</title>
+</head>
+<body>
+                       <!--para pasar la variable nombre al navegador -->
+	<h1>Saludos para <?php echo $nombre  ?></h1>
+</body>
+</html>
