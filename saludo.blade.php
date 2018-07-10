ecf020bb3d31c5238b7b3ab427158282044c95ae
gustavo1017 <gustavoeqr1017@gmail.com>
2018-07-09 20:58:10 -0500

    añadiendo el array a saludo.blade

--- saludo.blade.php
+++ saludo.blade.php
@@ -2,4 +2,14 @@
 
 @section('contenido')
 <h1>Saludos a {{$nombre}}</h1>
+
+<ul>
+	@forelse($consolas as $consola)
+	<li>{{$consola}}</li>
+	@empty
+	<p>No hay consolas</p>
+	@endforelse
+
+</ul>
+
 @stop
