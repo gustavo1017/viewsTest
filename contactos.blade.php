b37c33bbefa84b1f40c569b8bc19b8fc1c35721f
gustavo1017 <gustavoeqr1017@gmail.com>
2018-07-09 21:28:13 -0500

    crecion de formulario en contactos.blade

--- contactos.blade.php
+++ contactos.blade.php
@@ -1,4 +1,29 @@
 @extends('layout')
 @section('contenido')
 <h1>Contactos</h1>
+
+<h2>Escribeme</h2>
+<form method="POST" action="contacto">
+
+	<label for="nombre">
+		Nombre
+		<input type="text" name="nombre">
+
+	</label>
+	<label for="email">
+		Email
+		<input type="email" name="email">
+
+	</label>
+	<label for="mensaje">
+		Mensaje
+		<textarea name="mensaje" ></textarea>
+	</label>
+
+	<input type="submit" name="Enviar">
+	
+
+</form>
+
+
 @stop
