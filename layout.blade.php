7c7213bc8467feaab4b688421ebf142dca9a2438
gustavo1017 <gustavoeqr1017@gmail.com>
2018-07-09 21:23:20 -0500

    usar funcion activateMenu para limpiar el codigo

--- layout.blade.php
+++ layout.blade.php
@@ -14,12 +14,16 @@
 
 
 <header>
-	<h1>{{request()->is('/') ? 'Estas en el home':'No estas en el home' }}</h1>
+	<!--Solo por el ejemplo esta funcion no va aca -->
+	<?php function activeMenu($url){
+		return request()->is($url) ? 'active':'';
+	}
+	?>
 		<nav>
-			<a class=" {{request()->is('/')? 'active':''}} " href="{{ route('home') }}  ">Home</a>
+			<a class=" {{activeMenu('/')}} " href="{{ route('home') }}  ">Home</a>
 			<!-- * se puede escribir cualquier cosa en lugar del asterisco -->
-			<a class="{{request()->is('saludos/*')? 'active':''}}" href="{{route('saludos','Gustavo')}}">Saludos</a>
-			<a class="{{request()->is('contactame')? 'active':''}}" href="{{route('contactos') }}">Contacto</a>
+			<a class="{{activeMenu('saludos/*')}}" href="{{route('saludos','Gustavo')}}">Saludos</a>
+			<a class="{{activeMenu('contactame')}}" href="{{route('contactos') }}">Contacto</a>
 			
 
 		</nav>
