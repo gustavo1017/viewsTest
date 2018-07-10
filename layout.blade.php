0acaacb56f80c85a10838972428508a18401ff85
gustavo1017 <gustavoeqr1017@gmail.com>
2018-07-09 21:17:04 -0500

    * para poder escribircualquier nombre

--- layout.blade.php
+++ layout.blade.php
@@ -17,8 +17,8 @@
 	<h1>{{request()->is('/') ? 'Estas en el home':'No estas en el home' }}</h1>
 		<nav>
 			<a class=" {{request()->is('/')? 'active':''}} " href="{{ route('home') }}  ">Home</a>
-			
-			<a class="{{request()->is('saludos/gustavo')? 'active':''}}" href="{{route('saludos','Gustavo')}}">Saludos</a>
+			<!-- * se puede escribir cualquier cosa en lugar del asterisco -->
+			<a class="{{request()->is('saludos/*')? 'active':''}}" href="{{route('saludos','Gustavo')}}">Saludos</a>
 			<a class="{{request()->is('contactame')? 'active':''}}" href="{{route('contactos') }}">Contacto</a>
 			
 
